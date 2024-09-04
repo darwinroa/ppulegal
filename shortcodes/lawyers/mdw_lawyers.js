529c65627b8f686aec1dbbb6a0007adac10c7a12
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-04 16:04:18 -0500

    feat(filter): Agrega funcionalidad al filtro de opciones para abogados

---
+++ shortcodes/lawyers/mdw_lawyers.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function($) {
+  var page = 1; // Inicializando el paginado
+  var isLoadMore = false;
+  // Esto se ejecuta cuando se presiona sobre el botón de filtrar
+  // De modo que el filtro se realiza tomando los datos seleccionados
+  $('#mdw__button-filter-lawyers').on('click', function() {
+    page = 1; // Inicializando el paginado cada vez que se desea filtrar
+    isLoadMore = false;
+    mdwLawyersAjax(page);
+  })
+  
+  // Esto se ejecuta cuando se presiona sobre el botón de Load More
+  // Realizando una petición de más post.
+  // Considerando también los datos seleccionados para el filtro
+  $('#mdw__button-loadmore-lawyers').on('click', function() {
+    page++;
+    isLoadMore = true;
+    mdwLawyersAjax(page);
+  })
+  
+  // Función Ajax para la petición del filtro y el cargar más
+  function mdwLawyersAjax (page) {
+    const practiceArea = $('#areas-practica').val();
+    const country = $('#pais').val();
+    const rol = $('#roles').val();
+    console.log('Area de practoca', practiceArea);
+    console.log('País', country);
+    console.log('Rol', rol);
+    console.log('page', page);
+    $.ajax({
+      url: wp_ajax.ajax_url,
+      type: 'post',
+      data: {
+        action: 'mdw_lawyer_ajax_filter',
+        nonce: wp_ajax.nonce,
+        page,
+        practiceArea,
+        country,
+        rol
+      },
+      // beforeSend: function(){
+      //   const loaderUrl = wp_ajax.theme_directory_uri + '/inc/img/ri-preloader.svg';
+      //   const loaderIcon = `<div class='mdw-loader-ajax' bis_skin_checked='1'><img id='mdw__loadmore-icon' height='20' width='20' decoding='async' alt='Loading' data-src='${loaderUrl}' class='ls-is-cached lazyloaded e-font-icon-svg e-fas-spinner eicon-animation-spin' src='${loaderUrl}'></div>`;
+      //   isLoadMore ||  $('#mdw__lawyers_section .mdw__content_loop-grid').empty();
+      //   $('#mdw__lawyers_section .mdw__content-button-loadmore').append(loaderIcon);
+      //   $('#mdw__lawyers_section .mdw__button-loadmore').hide();
+      // },
+      success: function(response) {
+        if (response.success) {
+          $('#mdw__lawyers_section .mdw__button-loadmore').show();
+          $('.mdw-loader-ajax').remove();
+          if(isLoadMore) {
+            $('#mdw__lawyers_section .mdw__content_loop-grid').append(response.data);
+          } else {
+            $('#mdw__lawyers_section .mdw__content_loop-grid').html(response.data);
+          }
+        } else {
+            $('#mdw__lawyers_section .mdw__content_loop-grid').html('<p>Hubo un error en la solicitud.</p>');
+        }
+      }
+    })
+  }
+})
--- shortcodes/lawyers/mdw_lawyers.php
+++ shortcodes/lawyers/mdw_lawyers.php
@@ -5,6 +5,12 @@ if (!function_exists('mdw_lawyers_function')) {
   function mdw_lawyers_function()
   {
     wp_enqueue_style('mdw-lawyer-style', get_stylesheet_directory_uri() . '/shortcodes/lawyers/mdw_lawyers.css', array(), '1.0');
+    wp_enqueue_script('mdw-lawyer-script', get_stylesheet_directory_uri() . '/shortcodes/lawyers/mdw_lawyers.js', array('jquery'), null, true);
+    wp_localize_script('mdw-lawyer-script', 'wp_ajax', array(
+      'ajax_url'            => admin_url('admin-ajax.php'),
+      'nonce'               => wp_create_nonce('load_more_nonce'),
+      'theme_directory_uri' => get_stylesheet_directory_uri(),
+    ));
 
     /**
      * Aquí se optiene el Loop inicial al momento de cargar la web
@@ -35,7 +41,7 @@ if (!function_exists('mdw_lawyers_function')) {
         'name' => 'Rol'
       )
     );
-    $form_ID = 'mdw-filter-lawyers';
+    $form_ID = 'filter-lawyers';
     $filterHTML = mdw_html_filter_form($taxonomies, $form_ID);
     ob_start();
     $html = '';
@@ -72,4 +78,61 @@ function mdw_query_lawyers_loop($args)
   else : $html .= "<div class='mdw__without-results'>No more results</div>";
   endif;
   return $html;
+}
+
+/**
+ * Función para la respuesta del Ajax
+ */
+if (!function_exists('mdw_lawyer_ajax_filter')) {
+  add_action('wp_ajax_nopriv_mdw_lawyer_ajax_filter', 'mdw_lawyer_ajax_filter');
+  add_action('wp_ajax_mdw_lawyer_ajax_filter', 'mdw_lawyer_ajax_filter');
+
+  function mdw_lawyer_ajax_filter()
+  {
+    check_ajax_referer('load_more_nonce', 'nonce');
+    $page = $_POST['page'];
+    $practice_area = isset($_POST['practiceArea']) ? sanitize_text_field($_POST['practiceArea']) : '';
+    $country = isset($_POST['country']) ? sanitize_text_field($_POST['country']) : '';
+    $roles = isset($_POST['rol']) ? sanitize_text_field($_POST['rol']) : '';
+
+    /**
+     * Construyendo los argumentos necesarios para el Query
+     */
+    $tax_query = array('relation' => 'AND');
+    if ($practice_area) {
+      $tax_query[] =  array(
+        'taxonomy' => 'areas-practica',
+        'field' => 'term_id',
+        'terms' => intval($practice_area)
+      );
+    }
+    if ($country) {
+      $tax_query[] =  array(
+        'taxonomy' => 'pais',
+        'field' => 'term_id',
+        'terms' => intval($country)
+      );
+    }
+    if ($roles) {
+      $tax_query[] =  array(
+        'taxonomy' => 'roles',
+        'field' => 'term_id',
+        'terms' => intval($roles)
+      );
+    }
+    $post_per_page = 16;
+    $args = array(
+      'post_type' => 'bd-abogados',
+      'orderby' => 'title',
+      'order' => 'ASC',
+      'posts_per_page' => $post_per_page,
+      'tax_query' => $tax_query,
+      'paged' => $page
+    );
+    $query_loop = mdw_query_lawyers_loop($args);
+    $html = $query_loop;
+
+    wp_send_json_success($html);
+    wp_die();
+  }
 }
