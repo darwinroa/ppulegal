1aad6c6642fe37814288c16deac619da7cb9aeaa
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-09-15 20:53:52 -0500

    feat(single lawyers): Crea shortcode para imprimir las membresías del abogado

--- shortcodes/single_lawyers/mdw_single_lawyers.php
+++ shortcodes/single_lawyers/mdw_single_lawyers.php
@@ -65,7 +65,7 @@ function lawyer_academic_activities_function()
     $ocupation = $activity['cargo'];
 
     $html .= "
-      <div class='lawyer_academic_activities'>
+      <div class='mdw__lawyer_academic_activities'>
         <div class='mdw__activity_year mdw-fw300'>$year</div>
         <div class='mdw__activity_description mdw-fw400'>$description</div>
         <div class='mdw__activity_ocupation mdw-fw700'>$ocupation</div>
@@ -76,9 +76,9 @@ function lawyer_academic_activities_function()
 }
 
 
-add_shortcode('lawyer_language', 'lawyer_language');
+add_shortcode('lawyer_language', 'lawyer_language_function');
 
-function lawyer_language()
+function lawyer_language_function()
 {
   wp_enqueue_style('mdw-education-style', get_stylesheet_directory_uri() . '/shortcodes/single_lawyers/mdw_single_lawyers.css', array(), '1.0');
 
@@ -98,5 +98,77 @@ function lawyer_language()
       <div class='mdw__language_year mdw-fw400'>$htmlLanguage</div>
     </div>
   ";
+  return $html;
+}
+
+
+
+// add_shortcode('lawyer_posts', 'lawyer_posts_function');
+
+// function lawyer_posts_function()
+// {
+//   wp_enqueue_style('mdw-education-style', get_stylesheet_directory_uri() . '/shortcodes/single_lawyers/mdw_single_lawyers.css', array(), '1.0');
+
+//   // Obtener el ID del abogado actual (post ID en el single de bd-abogados)
+//   $lawyerId = get_the_ID();
+
+//   // Configurar la consulta para obtener los posts de ppulegal relacionados con este abogado
+//   $args = array(
+//     'post_type' => 'ppulegal',
+//     'meta_query' => array(
+//       array(
+//         'key' => 'abogados', // Nombre del ACF de relación
+//         'value' => '"' . $lawyerId . '"',
+//         'compare' => 'LIKE'
+//       )
+//     )
+//   );
+
+//   // Ejecutar la consulta
+//   $query = new WP_Query($args);
+
+//   // Verificar si hay publicaciones relacionadas
+//   if ($query->have_posts()) {
+//     $output = '<ul>';
+
+//     // Recorrer los posts y generar la salida
+//     while ($query->have_posts()) {
+//       $query->the_post();
+//       $output .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+//     }
+
+//     $output .= '</ul>';
+//   } else {
+//     $output = '<p>No hay publicaciones relacionadas.</p>';
+//   }
+
+//   // Restaurar el post original
+//   wp_reset_postdata();
+
+//   return $output;
+// }
+
+
+add_shortcode('lawyer_membership', 'lawyer_membership_function');
+
+function lawyer_membership_function()
+{
+  wp_enqueue_style('mdw-education-style', get_stylesheet_directory_uri() . '/shortcodes/single_lawyers/mdw_single_lawyers.css', array(), '1.0');
+
+  $memberships = get_field('membresias');
+  $html = '';
+  $membershipHTML = '';
+  if (! empty($memberships) && ! is_wp_error($memberships)) {
+    foreach ($memberships as $membership) {
+      $membershipLi = $membership['membresia'];
+      $membershipHTML .= "<li>$membershipLi</li>";
+    }
+
+    $html .= "
+          <div class='lawyer_membership'>
+            <ul class='mdw__membership mdw-fw300'>$membershipHTML</ul>
+          </div>
+        ";
+  }
   return $html;
 }
